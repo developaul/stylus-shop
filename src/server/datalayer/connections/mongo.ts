ac5b2053ad61c2e90f7bd30bd84389ce196bcbb9
Paul Chávez <paulchavezromero20@gmail.com>
2023-11-11 22:58:07 -0500

    fix: disconect process removed

--- src/server/datalayer/connections/mongo.ts
+++ src/server/datalayer/connections/mongo.ts
@@ -34,12 +34,4 @@ export const connect = async () => {
 
 export const disconnect = async () => {
 
-  if (process.env.NEXT_PHASE === 'phase-production-build' || process.env.NODE_ENV === 'development') return;
-
-  if (mongoConnection.isConnected === 0) return;
-
-  await mongoose.disconnect();
-  mongoConnection.isConnected = 0;
-
-  console.log('Desconectado de MongoDB');
 }
